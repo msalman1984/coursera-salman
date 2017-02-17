f6b5d5941e466522f94b617142202acb0f491eb7
salman <salman@example.com>
2017-02-17 21:58:04 +0100

    mod3-sol-init

---
+++ mod3-solution/app.js
@@ -0,0 +1,52 @@
+
+(function(){
+  angular.module('CheckOffList', [])
+
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
+    var ToBuy = this;
+    ToBuy.ToBuyArr = ShoppingListCheckOffService.getToBuyArr();
+
+    ToBuy.itemBought = function(idx){
+      ShoppingListCheckOffService.ItemBought(idx);
+    };
+  }
+
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService){
+    var Bought = this;
+    Bought.BoughtArr = ShoppingListCheckOffService.getBoughtArr();
+
+  }
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+
+    var ToBuyArr = [{name: "cookies", quantity: 10},{name: "Milk", quantity: 5},{name: "Meat", quantity: 2}
+                    ,{name: "Pizza", quantity: 2},{name: "Cola", quantity: 4}];
+    var BoughtArr = [];
+
+    service.getToBuyArr = function(){
+      return ToBuyArr;
+    };
+
+    service.getBoughtArr = function(){
+      return BoughtArr;
+    };
+
+    service.ItemBought = function(idx){
+      var _obj = {};
+      _obj.name = ToBuyArr[idx].name;
+      _obj.quantity = ToBuyArr[idx].quantity;
+      BoughtArr.push(_obj);
+      ToBuyArr.splice(idx,1);
+    };
+
+  }
+
+})();
